27e390023c8193e7972e7f570e980be0a72ef726
Kay Gosho <ketsume0211@gmail.com>
2022-07-04 23:22:29 +0900

    use Bear-03/vosk-rs sample

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,103 @@
-// mod vosk;
-// mod vosk_binding;
+use std::{
+    env,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
-use portaudio_rs::device::DeviceInfo;
-use portaudio_rs::stream::{Stream, StreamCallbackResult, StreamFlags, StreamParameters};
-use std::collections::BTreeMap;
-use vosk::{DecodingState, Model, Recognizer};
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    ChannelCount,
+};
+use dasp::{sample::ToSample, Sample};
+use vosk::{Model, Recognizer};
 
 fn main() {
-    // println!("{:?}", vosk::create_recognizer());
+    let mut args = env::args();
+    args.next();
 
-    println!("Hello, world!");
+    let model_path = args.next().expect("A model path was not provided");
+    let record_duration = Duration::from_secs(
+        args.next()
+            .expect("A recording duration was not provided")
+            .parse()
+            .expect("Invalid recording duration"),
+    );
 
-    let samples = vec![100, -2, 700, 30, 4, 5];
-    let model_path = "model";
+    let audio_input_device = cpal::default_host()
+        .default_input_device()
+        .expect("No input device connected");
 
-    let model = Model::new(model_path).unwrap();
-    let mut recognizer = Recognizer::new(&model, 16000.0).unwrap();
+    let config = audio_input_device
+        .default_input_config()
+        .expect("Failed to load default input config");
+    let channels = config.channels();
+
+    let model = Model::new(model_path).expect("Could not create the model");
+    let mut recognizer = Recognizer::new(&model, config.sample_rate().0 as f32)
+        .expect("Could not create the Recognizer");
 
     recognizer.set_max_alternatives(10);
     recognizer.set_words(true);
     recognizer.set_partial_words(true);
 
-    for sample in samples.chunks(100) {
-        recognizer.accept_waveform(sample);
-        println!("{:#?}", recognizer.partial_result());
-    }
+    let recognizer = Arc::new(Mutex::new(recognizer));
 
-    println!("{:#?}", recognizer.final_result().multiple().unwrap());
-
-    //  ----------------------- main -------------------------------
-
-    let devices = list_devices().expect("portaudio failed");
-
-    let i = portaudio_rs::device::get_default_input_index().expect("no default input");
-    println!("Using default input device {}", i);
-
-    let info = devices.get(&i).expect("no device info");
-
-    let mut last_partial = String::new();
-
-    let input_par = StreamParameters {
-        device: i,
-        channel_count: 1,
-        suggested_latency: info.default_low_input_latency,
-        data: 42, // random
+    let err_fn = move |err| {
+        eprintln!("an error occurred on stream: {}", err);
     };
-    let stream = Stream::open(
-        Some(input_par), // input channels
-        None,            // output channels
-        16000.0,         // sample rate
-        portaudio_rs::stream::FRAMES_PER_BUFFER_UNSPECIFIED,
-        StreamFlags::empty(),
-        Some(Box::new(move |input, _out: &mut [i16], _time, _flags| {
-            let status = recognizer.accept_waveform(input);
-            let result = recognizer.partial_result();
-            println!("{}", result.partial);
 
-            match status {
-                DecodingState::Running => {
-                    let result = recognizer.partial_result();
-                    // if result.partial != last_partial {
-                    //     last_partial.clear();
-                    //     last_partial.insert_str(0, &result.partial);
-                    //     if !result.partial.is_empty() {
-                    //         println!("{}", result.partial);
-                    //     }
-                    // }
-                    // println!("{:?}", result.partial_result);
-                }
-                DecodingState::Finalized => {
-                    let result = recognizer.final_result();
-                    if let Some(multi) = result.multiple() {
-                        let first = multi.alternatives.iter().next();
-                        // println!("{:?}", first);
-                    }
+    let recognizer_clone = recognizer.clone();
+    let stream = match config.sample_format() {
+        cpal::SampleFormat::F32 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[f32], _| recognize(&mut recognizer_clone.lock().unwrap(), data, channels),
+            err_fn,
+        ),
+        cpal::SampleFormat::U16 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[u16], _| recognize(&mut recognizer_clone.lock().unwrap(), data, channels),
+            err_fn,
+        ),
+        cpal::SampleFormat::I16 => audio_input_device.build_input_stream(
+            &config.into(),
+            move |data: &[i16], _| recognize(&mut recognizer_clone.lock().unwrap(), data, channels),
+            err_fn,
+        ),
+    }
+    .expect("Could not build stream");
 
-                    // let mut most_likely = None;
-                    //
-                    // while let Some(val) = multi.next() {
-                    //     if max_val.as_ref().map_or(true, |m| &val > m) {
-                    //         max_iter = self.clone();
-                    //         max_val = Some(val);
-                    //     }
-                    // }
+    stream.play().expect("Could not play stream");
+    println!("Recording...");
 
-                    // println!("{:?}", result);
-                }
-                _ => {}
-            }
+    std::thread::sleep(record_duration);
+    drop(stream);
 
-            if status == DecodingState::Finalized {}
-            StreamCallbackResult::Continue
-        })),
-    )
-    .unwrap();
-    stream.start().expect("failed to start the stream");
-    std::thread::park();
+    println!("{:#?}", recognizer.lock().unwrap().final_result());
 }
 
-fn list_devices() -> Result<BTreeMap<u32, DeviceInfo>, portaudio_rs::PaError> {
-    portaudio_rs::initialize()?;
-    let n = portaudio_rs::device::get_count()?;
-    let inputs = (0..n)
-        .into_iter()
-        .filter_map(|index| {
-            let info = portaudio_rs::device::get_info(index)?;
-            if info.max_input_channels > 0 {
-                Some((index, info))
-            } else {
-                None
-            }
-        })
-        .collect::<BTreeMap<_, _>>();
-    if inputs.is_empty() {
-        println!("No input devices found.");
+fn recognize<T: Sample + ToSample<i16>>(
+    recognizer: &mut Recognizer,
+    data: &[T],
+    channels: ChannelCount,
+) {
+    let data: Vec<i16> = data.iter().map(|v| v.to_sample()).collect();
+    let data = if channels != 1 {
+        stereo_to_mono(&data)
     } else {
-        println!("Input devices:");
-        for (index, info) in inputs.iter() {
-            println!("Index={} Name={}", index, info.name);
-        }
-    }
-    Ok(inputs)
+        data
+    };
+
+    recognizer.accept_waveform(&data);
+    println!("{:#?}", recognizer.partial_result());
+}
+
+pub fn stereo_to_mono(input_data: &[i16]) -> Vec<i16> {
+    let mut result = Vec::with_capacity(input_data.len() / 2);
+    result.extend(
+        input_data
+            .chunks_exact(2)
+            .map(|chunk| chunk[0] / 2 + chunk[1] / 2),
+    );
+
+    result
 }
